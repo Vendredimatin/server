71c0e2b713f3de6f04711a2feecb23b6a87b19c6
yujiezhang <845075906@qq.com>
2019-05-26 15:07:36 +0800

    eighth push

--- src/main/java/app/server/controller/student/StudentController.java
+++ src/main/java/app/server/controller/student/StudentController.java
@@ -3,6 +3,7 @@ package app.server.controller.student;
 import app.server.bean.Student;
 import app.server.service.CommentService;
 import app.server.service.StudentService;
+import app.server.util.LoggerUtil;
 import app.server.vo.CommentVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -15,14 +16,21 @@ public class StudentController {
     StudentService studentService;
     @PostMapping(value = "/login")//String username,String password
     public String login(@RequestBody Student student){
-        System.out.println("login");
-        System.out.println(student.getUsername());
-        System.out.println(student.getPassword());
+        LoggerUtil.loggerUtil.logInfo("login");
+        LoggerUtil.loggerUtil.logInfo(student.getUsername());
+        LoggerUtil.loggerUtil.logInfo(student.getPassword());
         return studentService.login(student.getUsername(),student.getPassword());
     }
 
     @PostMapping(value = "/register")//String username,String password,String school,String studentNumber
     public String register(@RequestBody Student student){
+        LoggerUtil.loggerUtil.logInfo("register");
+        LoggerUtil.loggerUtil.logInfo(student.getUsername());
+        LoggerUtil.loggerUtil.logInfo(student.getPassword());
+        LoggerUtil.loggerUtil.logInfo(student.getSchool());
+        LoggerUtil.loggerUtil.logInfo(student.getStudentNumber());
+        LoggerUtil.loggerUtil.logInfo(student.getPassword());
+
         return studentService.register(student.getUsername(),student.getPassword(),student.getSchool(),student.getStudentNumber());
     }
     @RequestMapping(value = "/commnet")
