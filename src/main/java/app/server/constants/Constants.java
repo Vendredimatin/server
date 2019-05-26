164acbd16693c538a5b7f5b911286e318cb682a8
Liu Hanyi <161250072@smail.nju.edu.cn>
2019-05-27 07:16:27 +0800

    commit constants

---
+++ src/main/java/app/server/constants/Constants.java
@@ -0,0 +1,12 @@
+package app.server.constants;
+
+/**
+ * @Author：Liu hanyi
+ * @Description：
+ * @Date Created in ${Time} ${Date}
+ * @Modified By:
+ */
+public class Constants {
+    public static final String SUC = "SUCCESS";
+    public static final String FAIL = "FAILURE";
+}
