35ecd2aecadb0be31a7aeb4f846ab774fa30d97a
Liu Hanyi <161250072@smail.nju.edu.cn>
2019-05-26 22:17:06 +0800

    commit test

--- src/main/java/app/server/bean/Collect.java
+++ src/main/java/app/server/bean/Collect.java
@@ -18,4 +18,28 @@ public class Collect {
     private int ID;
     private String username;
     private String courseId;
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(String courseId) {
+        this.courseId = courseId;
+    }
 }
