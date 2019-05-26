7f41410fb1742461d6b7c25e2537c7b83d86cf52
yujiezhang <845075906@qq.com>
2019-05-26 11:50:54 +0800

    second push

--- src/main/java/app/server/controller/student/studentController.java
+++ src/main/java/app/server/controller/student/studentController.java
@@ -1,17 +1,26 @@
 package app.server.controller.student;
 
+import app.server.bean.Student;
+import app.server.vo.CommentVO;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class studentController {
-    @RequestMapping("/login")
-    public String login(String username,String password){
+    @RequestMapping(value = "/login",method = RequestMethod.POST)//String username,String password
+    public String login(@RequestBody Student student){
+        System.out.println("login");
         return "Success";
     }
 
-    @RequestMapping("/register")
-    public String register(String username,String password,String school,String studentNumber){
+    @RequestMapping(value = "/register",method = RequestMethod.POST)//String username,String password,String school,String studentNumber
+    public String register(@RequestBody Student student){
+        return null;
+    }
+    @RequestMapping(value = "/commnet",method = RequestMethod.POST)
+    public String comment(@RequestBody CommentVO commentVO){
         return null;
     }
 }
