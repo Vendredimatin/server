9e6e0469d0c8e1646085c31a8c838dc4c8ec2b1a
yujiezhang <845075906@qq.com>
2019-05-26 17:38:34 +0800

    ninth push

--- src/main/java/app/server/vo/CourseVO.java
+++ src/main/java/app/server/vo/CourseVO.java
@@ -5,7 +5,6 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
-import java.util.List;
 
 @Data
 @NoArgsConstructor
@@ -14,7 +13,7 @@ public class CourseVO {
     private String iD;
     private String name;
     private String teacherName;
-    private List<Integer> scoreList = new ArrayList<>(5);
+    private ArrayList<Integer> scoreList = new ArrayList<>(5);
     boolean anonymous = false;
     boolean collect = false;
 }
