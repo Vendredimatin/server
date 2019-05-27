44c0f6375618e9974f787bdd323ae91ad2f344b9
Liu Hanyi <161250072@smail.nju.edu.cn>
2019-05-27 13:26:21 +0800

    modify students

--- src/main/java/app/server/constants/Constants.java
+++ src/main/java/app/server/constants/Constants.java
@@ -7,6 +7,9 @@ package app.server.constants;
  * @Modified By:
  */
 public class Constants {
+    private Constants() {
+    }
+
     public static final String SUC = "SUCCESS";
     public static final String FAIL = "FAILURE";
 }
